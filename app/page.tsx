086f664ee4be8dc615abcd516af4824c8559a54d
roiez1-60-seconds-hazardous-materials <roiez1@gmail.com>
2026-02-19 10:56:40 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+// This imports the main shift scheduler app
+// The full app component from the artifact is placed in src/app/ShiftScheduler.jsx
+import ShiftScheduler from './ShiftScheduler';
+
+export default function Home() {
+  return <ShiftScheduler />;
+}
